3635dfb1e82af34045a18bc72447c8b02cbeae00
avivashishta29 <avivashishta@gmail.com>
2023-08-11 23:57:37 +0530

    array

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let arr = [
-  { id: 1, name: 'john', age: '18', profession: 'developer' },
-  { id: 2, name: 'jack', age: '20', profession: 'developer' },
-  { id: 3, name: 'karen', age: '19', profession: 'admin' },
+  { id: 1, name: 'john', age: 18, profession: 'developer' },
+  { id: 2, name: 'jack', age: 20, profession: 'developer' },
+  { id: 3, name: 'karen', age: 19, profession: 'admin' },
 ];
 
 // Don't worry about consoling these functions, they are already being called on the button clicks.
