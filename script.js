dd698523562ad9545f36be2f20649c31839a7d18
avivashishta29 <avivashishta@gmail.com>
2023-08-11 23:08:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let arr = [
+  { id: 1, name: 'john', age: '18', profession: 'developer' },
+  { id: 2, name: 'jack', age: '20', profession: 'developer' },
+  { id: 3, name: 'karen', age: '19', profession: 'admin' },
+];
+
+// Don't worry about consoling these functions, they are already being called on the button clicks.
+// Please don't change anything in the index.html file.
+
+function PrintDeveloper() {
+  //Write your code here , just console.log
+}
+
+function addData() {
+  //Write your code here, just console.log
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+}
+
+// Here is an example of how functions work,
+// basically a function is a block of code which can directly access your 'arr' variable since arr is global.
+// If I have a function called consoleArr(), that can directly access arr like this to console it.
+
+function consoleArr() {
+  console.log('Consoling Array Variable', arr);
+  // Over here I can directly access the variable.
+}
