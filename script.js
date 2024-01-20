e3c235f36104a0bf02c992f2e9999b5f2849c6fc
Vivek Kumar <vickyvivek557@gmail.com>
2024-01-20 13:29:56 +0530

    completed

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
+/** @format */
+
 let arr = [
-  { id: 1, name: 'john', age: 18, profession: 'developer' },
-  { id: 2, name: 'jack', age: 20, profession: 'developer' },
-  { id: 3, name: 'karen', age: 19, profession: 'admin' },
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
-// Don't worry about consoling these functions, they are already being called on the button clicks.
-// Please don't change anything in the index.html file.
-
-function PrintDeveloper() {
+function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr
+  .filter((employee) => employee.profession === "developer")
+  .map((developer) => console.log(developer));
+}
+
+function PrintDeveloperbyForEach() {
+  //Write your code here , just console.log
+  arr.forEach((employee) => {
+    if(employee.profession === "developer"){
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(newEmployee);
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  const filteredArr = arr.filter((employee) => {
+   return employee.profession !== "admin"
+  });
+  console.log(filteredArr);
 }
+
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArray = [
+    { id: 4, name: "alice", age: "22", profession: "designer" },
+    { id: 5, name: "bob", age: "25", profession: "analyst" },
+    { id: 6, name: "dave", age: "28", profession: "manager" },
+  ];
+
+  const concatenatedArray = arr.concat(newArray);
+  console.log(concatenatedArray);
 }
 
-// Here is an example of how functions work,
-// basically a function is a block of code which can directly access your 'arr' variable since arr is global.
-// If I have a function called consoleArr(), that can directly access arr like this to console it.
-
-function consoleArr() {
-  console.log('Consoling Array Variable', arr);
-  // Over here I can directly access the variable.
-}
